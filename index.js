c039999692094deb0b33731bdb6d4d45b23c3ffe
webdevterri <32005382+webdevterri@users.noreply.github.com>
2022-03-21 23:09:20 -0500

    Fixed current total after remove & enable button

--- index.js
+++ index.js
@@ -7,21 +7,24 @@ const btnSendInvoice = document.getElementById('btnSendInvoice');
 
 let currentTotal = 0
 
+// ADDING NOTE MESSAGE
 btnRow.addEventListener("click", () => {
   notesMessage.innerHTML = `<p class="note-copy">We accept cash, credit card or PayPal.</p>`
 })
+
+// SERVICE BUTTONS
 
 btnWashCar.disabled = false;
 
 btnWashCar.addEventListener("click", () => {
   // adds service under task
   document.getElementById('added-service').innerHTML += `
-  <div class="service-added">
-  <div class="service-right-col">
-  <div class="service-name">Wash Car</div>
-  <p class="remove-option">Remove</p>
-  </div>
-  <div class="service-cost"><span class="dollar-sign">$</span>10</div>
+  <div class="service-added" id="wash-car-service">
+    <div class="service-right-col">
+      <div class="service-name">Wash Car</div>
+      <p class="remove-option" id="remove-wash">Remove</p>
+    </div>
+    <div class="service-cost"><span class="dollar-sign">$</span>10</div>
   </div>
   `
   // adds to current total
@@ -29,7 +32,17 @@ btnWashCar.addEventListener("click", () => {
   document.getElementById("calculated-total").textContent = `$${currentTotal}`
   // Disables the button from being clicked again
   btnWashCar.disabled = true;
+
+  // REMOVE FUNCTIONALITY
+  document.getElementById('remove-wash').addEventListener("click", ()=> {
+    const removeWashTask = document.getElementById('wash-car-service');
+    removeWashTask.outerHTML = "";
+    currentTotal = currentTotal - 10;
+    document.getElementById("calculated-total").textContent = `$${currentTotal}`
+    btnWashCar.disabled = false;
+  })
 })
+
 
 btnMowLawn.addEventListener("click", () => {
   // adds service under task
@@ -73,3 +86,8 @@ document.getElementById('notesText').innerHTML = `<p class="note-copy">We accept
 btnSendInvoice.addEventListener("click", () => {
   console.log("This button doesn't actuall do anything!")
 })
+
+
+
+// TODO: add functionality to REMOVE text
+// TODO: make more DRY by putting add to current total inside a function (input the service's price when running the function) -- test with one button first!
